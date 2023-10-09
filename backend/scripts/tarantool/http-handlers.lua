3536a5367a9ca6b9a8ac44a942548c97f7895514
i.serbaev <i.serbaev@kryptonite.ru>
2023-10-09 20:05:45 +0300

    conversation: private_message_log tarantool http handlers

--- backend/scripts/tarantool/http-handlers.lua
+++ backend/scripts/tarantool/http-handlers.lua
@@ -1,32 +1,59 @@
 local exports = {}
 
+json = require('json')
+log = require('log')
+datetime = require('datetime')
+uuid = require('uuid')
+
 exports.test_handler = function(req)
     -- req is a Request object
     -- resp is a Response object
-    local resp = req:render({text = req.method..' '..req.path })
+    local resp = req:render({ text = req.method .. ' ' .. req.path })
     resp.headers['x-test-header'] = 'test';
     resp.status = 201
     return resp
 end
 
---exports.add_dialog = function(message_from, message_to, message)
---    box.space.private_message_log.insert()
---
---    box.space.conversation_log.select()
---
---    if err ~= nil then
---        log.error(err)
---        return nil
---    end
---
---    return result
---end
---
---exports.list_dialogs = function(user_id)
---    local bucket_id = vshard.router.bucket_id_mpcrc32(login)
---
---    local user = vshard.router.callbro(bucket_id, 'box.space.user_info:get', {login})
---    return user
---end
+exports.add_dialog = function(req)
+    -- req is a Request object
+    -- resp is a Response object
+    local dialog = req:json()
+    log.info("Received dialog")
+    log.info(dialog)
+
+    box.space.private_message_log:insert {
+        uuid.fromstr(dialog.conversation_id),
+        tonumber(dialog.conversation_index),
+        uuid.fromstr(dialog.message_from),
+        uuid.fromstr(dialog.message_to),
+        dialog.message,
+        datetime.new()
+    }
+    log.info("Dialog saved success")
+
+    local resp = req:render { text = "" }
+    resp.status = 201
+    return resp
+end
+
+exports.list_dialogs = function(req)
+    local conversation_id = req:stash('conversation_id')
+    log.info("Received request to list dialogs")
+    log.info(conversation_id)
+    local dialogs = box.space.private_message_log.index.primary:select(uuid.fromstr(conversation_id))
+
+    local resp = req:render({json = json.encode(dialogs)})
+    resp.status = 200
+    return resp
+end
+
+exports.all_dialogs = function(req)
+    log.info("Received request to list all dialogs")
+    local dialogs = box.space.private_message_log.index.primary:select()
+
+    local resp = req:render({json = json.encode(dialogs)})
+    resp.status = 200
+    return resp
+end
 
 return exports
