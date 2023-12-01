590e8124c9c666ee7769a276b4115bfd6ff7cd1d
i.serbaev <i.serbaev@kryptonite.ru>
2023-12-01 19:18:56 +0300

    digital_wallet: add endpoints

---
+++ backend/digital_wallet/src/main/scala/ru/nh/digital_wallet/http/DWEndpointDescriptions.scala
@@ -0,0 +1,42 @@
+package ru.nh.digital_wallet
+package http
+
+import cats.effect.IO
+import org.typelevel.log4cats.{ Logger, LoggerFactory }
+import ru.nh.digital_wallet.TransferCommand
+import ru.nh.http._
+import sttp.tapir._
+import sttp.tapir.json.circe.jsonBody
+import sttp.tapir.server.http4s.serverSentEventsBody
+
+class DWEndpointDescriptions(implicit L: LoggerFactory[IO]) {
+  import DWJson._
+
+  val resource: String                  = "v1/wallet"
+  val resourcePath: EndpointInput[Unit] = resource
+
+  implicit val log: Logger[IO] = L.getLoggerFromClass(classOf[DWEndpointDescriptions])
+
+  private val endpoint = baseEndpoint(resource, resourcePath)
+
+  val transferCmdPost: BaseEndpoint[TransferCommand, TransferCommandResponse] =
+    endpoint.post
+      .in("balance_transfer_command")
+      .in(jsonBody[TransferCommand])
+      .out(jsonBody[TransferCommandResponse])
+
+  val transferEventPost: BaseEndpoint[TransferEvent, BalanceSnapshot] =
+    endpoint.post
+      .in("balance_transfer_event")
+      .in(jsonBody[TransferEvent])
+      .out(jsonBody[BalanceSnapshot])
+
+  val accountBalanceStream =
+    endpoint.get
+      .in("balance" / "stream")
+      .in(path[String]("account_id"))
+      .out(NoCacheControlHeader)
+      .out(XAccelBufferingHeader)
+      .out(serverSentEventsBody[IO])
+
+}
---
+++ backend/digital_wallet/src/main/scala/ru/nh/digital_wallet/http/DWEndpoints.scala
@@ -0,0 +1,67 @@
+package ru.nh.digital_wallet.http
+
+import cats.data.NonEmptyList
+import cats.effect.IO
+import cats.syntax.all._
+import org.typelevel.log4cats.{ Logger, LoggerFactory }
+import ru.nh.digital_wallet.WalletService
+import ru.nh.http._
+import sttp.model.StatusCode
+
+import scala.concurrent.duration.FiniteDuration
+
+class DWEndpoints(
+    service: WalletService,
+    val sseHeartbeatPeriod: FiniteDuration
+)(
+    implicit L: LoggerFactory[IO]
+) extends SseSupport {
+
+  implicit val log: Logger[IO] = L.getLoggerFromClass(classOf[DWEndpoints])
+
+  private val endpointDescriptions = new DWEndpointDescriptions()
+
+  val transferCmdPost: SEndpoint = endpointDescriptions.transferCmdPost
+    .serverLogic { cmd =>
+      service
+        .publishTransferCommand(cmd)
+        .map {
+          _.leftMap {
+            case _: IllegalArgumentException => (StatusCode.BadRequest, none)
+            case ex =>
+              (StatusCode.InternalServerError, ErrorResponse(ex.getMessage, cmd.transactionId.toString, 0).some)
+          }
+        }
+    }
+
+  val transferEventPost: SEndpoint = endpointDescriptions.transferEventPost
+    .serverLogic { event =>
+      service
+        .publishTransferEvent(event)
+        .map {
+          _.leftMap {
+            case _: IllegalArgumentException => (StatusCode.BadRequest, none)
+            case ex =>
+              (StatusCode.InternalServerError, ErrorResponse(ex.getMessage, event.transactionId.toString, 0).some)
+          }
+        }
+    }
+
+  val accountBalanceStream: SEndpoint = endpointDescriptions.accountBalanceStream
+    .serverLogicSuccess { accountId =>
+      log
+        .debug(s"Start http account balance stream for [${accountId}]") *>
+        IO.delay {
+          service
+            .balanceStream(accountId)
+            .through(ssePipe)
+            .onFinalizeCase(ec => log.debug(s"Finalized account balance stream for [${accountId}], $ec"))
+        }
+    }
+
+  val all: NonEmptyList[SEndpoint] = NonEmptyList.of(
+    transferCmdPost,
+    transferEventPost,
+    accountBalanceStream
+  )
+}
